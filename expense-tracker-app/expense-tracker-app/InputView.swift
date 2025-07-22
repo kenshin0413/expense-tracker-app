78ecc6065357e812e9280f3d08594de52662c1d3
kenshin0413 <kenshin1234569@gmail.com>
2025-07-22 21:34:24 +0900

    UI作成

--- expense-tracker-app/expense-tracker-app/InputView.swift
+++ expense-tracker-app/expense-tracker-app/InputView.swift
@@ -8,10 +8,77 @@
 import SwiftUI
 
 struct InputView: View {
+    enum ExpenseType: String, CaseIterable {
+        case income = "収入"
+        case expense = "支出"
+    }
+    enum IncomeCategory: String, CaseIterable {
+        case pay = "給与"
+        case subjob = "副業"
+        case investment = "投資"
+        case other = "その他"
+    }
+    enum ExpenseCategory: String, CaseIterable {
+        case food = "食費"
+        case travel = "交通費"
+        case amusement = "娯楽"
+        case lightheat = "光熱費"
+        case medical = "医療費"
+        case other = "その他"
+    }
+    @State private var expenseType: ExpenseType = .expense
+    @State private var amount: String = ""
+    @State private var selectIncomeCategory: IncomeCategory = .pay
+    @State private var selectExpenseCategory: ExpenseCategory = .food
+    @State private var date: Date = Date()
     var body: some View {
         NavigationStack {
             Form {
-                Text("収支入力画面")
+                Section(header: Text("収支種別")) {
+                    Picker("", selection: $expenseType) {
+                        ForEach(ExpenseType.allCases, id: \.self) { type in
+                            Text(type.rawValue)
+                        }
+                    }
+                    .pickerStyle(.segmented)
+                }
+                
+                Section(header: Text("金額")) {
+                    TextField("金額を入力", text: $amount)
+                        .keyboardType(.decimalPad)
+                }
+                
+                Section(header: Text("カテゴリ")) {
+                    if expenseType == .income {
+                        Picker("カテゴリ", selection: $selectIncomeCategory) {
+                            ForEach(IncomeCategory.allCases, id: \.self) { type in
+                                Text(type.rawValue)
+                            }
+                        }
+                        .pickerStyle(.menu)
+                    } else {
+                        Picker("カテゴリ", selection: $selectExpenseCategory) {
+                            ForEach(ExpenseCategory.allCases, id: \.self) { type in
+                                Text(type.rawValue)
+                            }
+                        }
+                        .pickerStyle(.menu)
+                    }
+                }
+                
+                Section(header: Text("日付")) {
+                    DatePicker("日付", selection: $date,
+                               displayedComponents: [.date])
+                }
+                
+                Section {
+                    Button {
+                        
+                    } label: {
+                        Text("保存")
+                    }
+
+                }
             }
             .navigationTitle("収支入力画面")
         }
@@ -21,3 +88,4 @@ struct InputView: View {
 #Preview {
     InputView()
 }
+
